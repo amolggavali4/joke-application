6f9f231e926989113e69f183e9d24307201b21d7
Amol <amol.g.gavali@gmail.com>
2020-04-20 22:49:05 +0530

    setting log level to "trace" to log mappings

--- src/main/java/com/example/jokeapplication/config/JokeAppConfig.java
+++ src/main/java/com/example/jokeapplication/config/JokeAppConfig.java
@@ -1,7 +1,6 @@
 package com.example.jokeapplication.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
